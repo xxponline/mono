d2889595350a2ec0bded00c492f748d71c722b7c
Martin Baulig <martin.baulig@xamarin.com>
2015-01-07 12:44:23 +0100

    [corlib] System.Security.Cryptographic changes conditional to `INSIDE_MONO_SECURITY'.
    * Add new internal `IRunningHash' interface.
    
    * MD5CryptoServiceProvider, SHA1CryptoServiceProvider, SHA256Managed, SHA384Managed,
      SHA512Managed: when `INSIDE_MONO_SECURITY', make internal and implement `IRunningHash'.
    
    * RSA.cs: Add internal static `GetNamedParam'.
    
    (cherry picked from commit cf7a4327d1eeb7114c2ff206a42b14443c40eeb7)

--- mcs/class/corlib/System.Security.Cryptography/SHA512Managed.cs
+++ mcs/class/corlib/System.Security.Cryptography/SHA512Managed.cs
@@ -35,8 +35,16 @@ using System.Runtime.InteropServices;
 namespace System.Security.Cryptography {
 	
 [ComVisible (true)]
-public class SHA512Managed : SHA512 {
-
+#if INSIDE_MONO_SECURITY
+internal
+#else
+public
+#endif
+class SHA512Managed : SHA512
+#if INSIDE_MONO_SECURITY
+	, IRunningHash
+#endif
+{
 	private byte[] xBuf;
 	private int xBufOff;
 
@@ -54,6 +62,42 @@ public class SHA512Managed : SHA512 {
 		W = new ulong [80];
 		Initialize (false); // limited initialization
 	}
+
+#if INSIDE_MONO_SECURITY
+	static readonly byte[] empty = new byte [0];
+
+	SHA512Managed (SHA512Managed other)
+	{
+		xBuf = new byte [other.xBuf.Length];
+		Array.Copy (other.xBuf, xBuf, xBuf.Length);
+		xBufOff = other.xBufOff;
+		byteCount1 = other.byteCount1;
+		byteCount2 = other.byteCount2;
+		H1 = other.H1;
+		H2 = other.H2;
+		H3 = other.H3;
+		H4 = other.H4;
+		H5 = other.H5;
+		H6 = other.H6;
+		H7 = other.H7;
+		H8 = other.H8;
+		W = new ulong [other.W.Length];
+		Array.Copy (other.W, W, W.Length);
+		wOff = other.wOff;
+	}
+
+	void IRunningHash.TransformBlock (byte[] inputBuffer, int inputOffset, int inputCount)
+	{
+		TransformBlock (inputBuffer, inputOffset, inputCount, null, 0);
+	}
+
+	byte[] IRunningHash.GetRunningHash ()
+	{
+		var copy = new SHA512Managed (this);
+		copy.TransformFinalBlock (empty, 0, 0);
+		return copy.Hash;
+	}
+#endif
 
 	private void Initialize (bool reuse) 
 	{
