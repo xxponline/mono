19a61b18a52ac300a3047ced23bc080f198eab44
Martin Baulig <martin.baulig@xamarin.com>
2015-01-08 20:53:30 +0100

    And it builds!

--- mcs/class/Mono.Security/MartinsPlayground/Properties/AssemblyInfo-NewMonoSource.cs
+++ mcs/class/Mono.Security/MartinsPlayground/Properties/AssemblyInfo-NewMonoSource.cs
@@ -7,21 +7,13 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTitle("SecurityPlayground")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("martin")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
-// The form "{Major}.{Minor}.*" will automatically update the build and revision,
-// and "{Major}.{Minor}.{Build}.*" will update just the revision.
-
-[assembly: AssemblyVersion("1.0.*")]
-
-// The following attributes are used to specify the signing key for the assembly,
-// if desired. See the Mono documentation for more information about signing.
-
-//[assembly: AssemblyDelaySign(false)]
-//[assembly: AssemblyKeyFile("")]
+[assembly: AssemblyCompany (Consts.MonoCompany)]
+[assembly: AssemblyProduct (Consts.MonoProduct)]
+[assembly: AssemblyCopyright (Consts.MonoCopyright)]
+[assembly: AssemblyVersion (Consts.FxVersion)]
+[assembly: AssemblyFileVersion (Consts.FxFileVersion)]
+[assembly: AssemblyInformationalVersion (Consts.FxFileVersion)]
 
