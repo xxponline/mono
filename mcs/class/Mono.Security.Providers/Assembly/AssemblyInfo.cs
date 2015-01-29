97f9e61ec756302f330cfb748c889aeef6a2d420
Martin Baulig <martin.baulig@xamarin.com>
2015-01-29 20:22:53 +0100

    Add XS solution and .csproj.
    These will reference the installed libraries, not the newly compiled ones.

--- mcs/class/Mono.Security.Providers/Assembly/AssemblyInfo.cs
+++ mcs/class/Mono.Security.Providers/Assembly/AssemblyInfo.cs
@@ -44,6 +44,3 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible (false)]
 [assembly: NeutralResourcesLanguage ("en-US")]
 
-[assembly: AssemblyDelaySign (true)]
-[assembly: AssemblyKeyFile ("../../mono.pub")]
-
