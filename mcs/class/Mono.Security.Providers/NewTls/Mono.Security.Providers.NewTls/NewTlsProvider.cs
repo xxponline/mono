efe4020335eab4f34af2cfb61444b03f212396e7
Martin Baulig <martin.baulig@xamarin.com>
2015-01-30 04:23:30 +0100

    Add Mono.Security.Providers.NewTls.

---
+++ mcs/class/Mono.Security.Providers/NewTls/Mono.Security.Providers.NewTls/NewTlsProvider.cs
@@ -0,0 +1,81 @@
+//
+// NewTlsProvider.cs
+//
+// Author:
+//       Martin Baulig <martin.baulig@xamarin.com>
+//
+// Copyright (c) 2015 Xamarin, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using Mono.Security.Protocol.Tls;
+using Mono.Security.Interface;
+
+namespace Mono.Security.Providers
+{
+	/*
+	 * This provider only uses the public .NET APIs from System.dll.
+	 * 
+	 * It is primarily intended for testing.
+	 */
+	public class NewTlsProvider : MonoTlsProvider
+	{
+		public override bool SupportsHttps {
+			get { return false; }
+		}
+
+		public override bool SupportsSslStream {
+			get { return true; }
+		}
+
+		public override bool IsHttpsStream (Stream stream)
+		{
+			return false;
+		}
+
+#pragma warning disable 618
+		public override IMonoHttpsStream GetHttpsStream (Stream stream)
+#pragma warning restore 618
+		{
+			throw new InvalidOperationException ();
+		}
+
+#pragma warning disable 618
+		public override IMonoHttpsStream CreateHttpsClientStream (
+			Stream innerStream, X509CertificateCollection clientCertificates, HttpWebRequest request, byte[] buffer,
+			CertificateValidationCallback2	validationCallback)
+#pragma warning restore 618
+		{
+			throw new NotSupportedException ();
+		}
+
+		public override MonoSslStream CreateSslStream (
+			Stream innerStream, bool leaveInnerStreamOpen,
+			RemoteCertificateValidationCallback userCertificateValidationCallback,
+			LocalCertificateSelectionCallback userCertificateSelectionCallback)
+		{
+			throw new NotImplementedException ();
+		}
+	}
+}
+
