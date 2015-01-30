abc2cd22cec54cfc6174a4ef788cc6c3a058c151
Martin Baulig <martin.baulig@xamarin.com>
2015-01-30 04:43:43 +0100

    Move into the partial extension part.

--- mcs/class/System/ReferenceSources/SslStream.cs
+++ mcs/class/System/ReferenceSources/SslStream.cs
@@ -3,9 +3,26 @@
 //
 namespace System.Net.Security {
     using System.Net.Sockets;
+    using System.IO;
 
     partial class SslStream
     {
+        SSPIConfiguration _Configuration;
+
+        internal SslStream(Stream innerStream, bool leaveInnerStreamOpen, RemoteCertificateValidationCallback userCertificateValidationCallback, 
+            LocalCertificateSelectionCallback userCertificateSelectionCallback, EncryptionPolicy encryptionPolicy, SSPIConfiguration config)
+            : base(innerStream, leaveInnerStreamOpen)
+        {
+            if (encryptionPolicy != EncryptionPolicy.RequireEncryption && encryptionPolicy != EncryptionPolicy.AllowNoEncryption && encryptionPolicy != EncryptionPolicy.NoEncryption) 
+                throw new ArgumentException(SR.GetString(SR.net_invalid_enum, "EncryptionPolicy"), "encryptionPolicy");
+
+            _userCertificateValidationCallback = userCertificateValidationCallback;
+            _userCertificateSelectionCallback  = userCertificateSelectionCallback;
+            RemoteCertValidationCallback _userCertValidationCallbackWrapper = new RemoteCertValidationCallback(userCertValidationCallbackWrapper);
+            LocalCertSelectionCallback   _userCertSelectionCallbackWrapper  = userCertificateSelectionCallback==null  ? null : new LocalCertSelectionCallback(userCertSelectionCallbackWrapper);
+            _SslState = new SslState(innerStream, _userCertValidationCallbackWrapper, _userCertSelectionCallbackWrapper, encryptionPolicy, config);
+        }
+
         internal bool IsClosed
         {
             get { return _SslState.IsClosed; }
