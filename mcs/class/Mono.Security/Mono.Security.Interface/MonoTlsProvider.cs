30c5440157a913795256ba3fe8219b150f68ecc0
Martin Baulig <martin.baulig@xamarin.com>
2015-02-07 22:38:03 +0100

    [Mono.Security]: Add more 'Mono.Security.Interface' APIs.
    These new APIs are public to avoid making Mono.Security.dll's internals
    visible to the new TLS code, but they are likely going to change, so
    user code should not rely on them.
    
    * 'MonoTlsSettings': New empty abstract class.
    
    * 'IMonoHttpsStream': This interface reflects the way how Mono's current
      web stack accesses the TLS code.  Since that's using the obsolete
      'HttpsClientStream' class, this interface is flagged as [Obsolete]
      and will be removed once that code is cleaned up.
    
    * 'MonoTlsProvider': Abstraction model to create an instance of
      'MonoSslStream' and 'IMonoHttpsStream'.

---
+++ mcs/class/Mono.Security/Mono.Security.Interface/MonoTlsProvider.cs
@@ -0,0 +1,134 @@
+//
+// MonoTlsProvider.cs
+//
+// Author:
+//       Martin Baulig <martin.baulig@xamarin.com>
+//
+// Copyright (c) 2015 Xamarin, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using Mono.Security.Protocol.Tls;
+
+namespace Mono.Security.Interface
+{
+	/*
+	 * Unfortunately, we can't use the public definitions from System.dll here, so we need to
+	 * copy these.
+	 *
+	 * The @MonoRemoteCertificateValidationCallback also has an additional 'targetHost' argument.
+	 *
+	 */
+#if !INSIDE_SYSTEM
+	public
+#endif
+	enum MonoEncryptionPolicy
+	{
+		// Prohibit null ciphers (current system defaults)
+		RequireEncryption = 0,
+
+		// Add null ciphers to current system defaults
+		AllowNoEncryption,
+
+		// Request null ciphers only
+		NoEncryption
+	}
+
+#if !INSIDE_SYSTEM
+	public
+#endif
+	delegate bool MonoRemoteCertificateValidationCallback (
+		string targetHost, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors);
+
+#if !INSIDE_SYSTEM
+	public
+#endif
+	delegate X509Certificate MonoLocalCertificateSelectionCallback (
+		string targetHost, X509CertificateCollection localCertificates, X509Certificate remoteCertificate,
+		string[] acceptableIssuers);
+
+#if !INSIDE_SYSTEM
+	public
+#endif
+	abstract class MonoTlsProvider
+	{
+#region HttpsClientStream
+
+		/*
+		 * This section reflects how Mono's web stack currently accesses
+		 * the TLS code.  Since that's currently using the obsolete @HttpsClientStream,
+		 * this section needs to be cleaned up.  It is therefor marked as [Obsolete].
+		 *
+		 * It is only used in WebClient.cs.
+		 *
+		 */
+
+		public abstract bool SupportsHttps {
+			get;
+		}
+
+		#pragma warning disable 618
+		public abstract bool IsHttpsStream (Stream stream);
+
+		public abstract IMonoHttpsStream GetHttpsStream (Stream stream);
+
+		public abstract IMonoHttpsStream CreateHttpsClientStream (
+			Stream innerStream, HttpWebRequest request, byte[] buffer);
+		#pragma warning restore
+
+#endregion
+
+#region SslStream
+
+		/*
+		 * This section abstracts the @SslStream class.
+		 *
+		 */
+
+		public abstract bool SupportsSslStream {
+			get;
+		}
+
+		/*
+		 * Whether or not this TLS Provider supports Mono-specific extensions
+		 * (via @MonoTlsSettings).
+		 */
+		public abstract bool SupportsMonoExtensions {
+			get;
+		}
+
+		/*
+		 * Obtain a @MonoSslStream instance.
+		 *
+		 * The 'settings' argument is ignored unless this provider 'SupportsMonoExtensions'.
+		 */
+		public abstract MonoSslStream CreateSslStream (
+			Stream innerStream, bool leaveInnerStreamOpen,
+			MonoRemoteCertificateValidationCallback userCertificateValidationCallback,
+			MonoLocalCertificateSelectionCallback userCertificateSelectionCallback,
+			MonoTlsSettings settings = null);
+
+#endregion
+	}
+}
